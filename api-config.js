060a9c6cf6354db3e07f3946b03b267f514db826
jokemoomovieluckwheel <jokemoomovieluckwheel@gmail.com>
2026-02-25 12:09:32 +0700

    Update api-config.js

--- api-config.js
+++ api-config.js
@@ -4,8 +4,9 @@
  */
 (function () {
     'use strict';
-    window.WHEEL_API_BASE = 'https://jokemoomovieluckwheel.github.io/codejoke/';
+    window.WHEEL_API_BASE = 'https://script.google.com/macros/s/AKfycby4HdNu20AJz-3JdpJP1v1-xF1_fqVRkifSZwkly4PTzuq9A978T-UjMJNAZg06RIo3eA/exec';
     // ลิงก์ไปหน้าวงล้อ (ถ้าโฮสต์คนละโดเมน ใส่ URL เต็ม เช่น https://example.com/วงล้อ/)
     window.LINK_WHEEL = 'https://jokemoomovieluckwheel.github.io/wheelnew/';
 })();
+
 
